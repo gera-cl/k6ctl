3aaac15f80821db7bf3b32bad5a30b0ee64c0400
Alejandro Arevalo Sanchez <aoarevalo@falabella.cl>
2026-04-13 11:17:34 -0400

    Add logic to delete specific resources (pods, configmaps, testruns)

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -5,27 +5,48 @@ import logger from '../utils/logger';
 interface DeleteOptions {
   namespace?: string;
   keepConfigmap?: boolean;
+  pod?: string;
+  testrun?: string;
+  configmap?: string;
 }
 
 export async function deleteLastRun(options: DeleteOptions) {
-  const lastRun = await loadLastRun();
+  const kubernetesService = createDefaultKubernetesService();
+
+  const lastRun = !options.namespace ? await loadLastRun() : null;
+  const namespace = options.namespace ?? lastRun?.namespace ?? 'default';
+
+  if (options.pod) {
+    logger.info(`Deleting Pod: ${options.pod} (namespace: ${namespace})`);
+    await kubernetesService.deletePodByName(options.pod, namespace);
+    return;
+  }
+
+  if (options.testrun) {
+    logger.info(`Deleting TestRun: ${options.testrun} (namespace: ${namespace})`);
+    await kubernetesService.deleteTestRunByName(options.testrun, namespace);
+    return;
+  }
+
+  if (options.configmap) {
+    logger.info(`Deleting ConfigMap: ${options.configmap} (namespace: ${namespace})`);
+    await kubernetesService.deleteConfigMap(options.configmap, namespace);
+    return;
+  }
+
   if (!lastRun) {
     logger.error('No last run found. Run a test first with: k6ctl run <script>');
     process.exit(1);
   }
 
-  const namespace = options.namespace ?? lastRun.namespace;
   logger.info(`Deleting TestRun: ${lastRun.testRunName} (namespace: ${namespace})`);
-
-  const kubernetesService = createDefaultKubernetesService();
-
   await kubernetesService.deleteTestRunByName(lastRun.testRunName, namespace);
 
   if (!options.keepConfigmap) {
-    logger.info(`Deleting ConfigMap: ${lastRun.configMapName} (namespace: ${namespace})`);
-    await kubernetesService.deleteConfigMap(lastRun.configMapName, namespace);
+      logger.info(`Deleting ConfigMap: ${lastRun.configMapName} (namespace: ${namespace})`);
+      await kubernetesService.deleteConfigMap(lastRun.configMapName, namespace);
   }
 
-  await clearLastRun();
-  logger.info('Last run state cleared.');
+    await clearLastRun();
+    logger.info('Last run state cleared.');
 }
--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,26 +1,26 @@
 import logger, { setLogLevel } from '../utils/logger';
-import { createDefaultKubernetesService } from '../services/kubernetes.service';
+import { createDefaultKubernetesService, printPodsTable, printTestRunsTable, printConfigMapsTable } from '../services/kubernetes.service';
 
 export async function list(type: string, options: { namespace?: string }) {
   const kubernetesService = createDefaultKubernetesService();
   switch (type) {
     case 'all':
       logger.debug(`Listing all resources in namespace: ${options.namespace}`);
-      await kubernetesService.listTestRuns(options.namespace);
-      await kubernetesService.listPods(options.namespace);
-      await kubernetesService.listConfigMaps(options.namespace);
+      printTestRunsTable(await kubernetesService.listTestRuns(options.namespace));
+      printPodsTable(await kubernetesService.listPods(options.namespace));
+      printConfigMapsTable(await kubernetesService.listConfigMaps(options.namespace));
       break;
     case 'pods':
       logger.debug(`Listing Pods in namespace: ${options.namespace}`);
-      await kubernetesService.listPods(options.namespace);
+      printPodsTable(await kubernetesService.listPods(options.namespace));
       break;
     case 'testruns':
       logger.debug(`Listing TestRuns in namespace: ${options.namespace}`);
-      await kubernetesService.listTestRuns(options.namespace);
+      printTestRunsTable(await kubernetesService.listTestRuns(options.namespace));
       break;
     case 'configmaps':
       logger.debug(`Listing ConfigMaps in namespace: ${options.namespace}`);
-      await kubernetesService.listConfigMaps(options.namespace);
+      printConfigMapsTable(await kubernetesService.listConfigMaps(options.namespace));
       break;
     default:
       logger.debug(`Listing ${type} in namespace: ${options.namespace}`);
